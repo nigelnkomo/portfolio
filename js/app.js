c469d5bd57d52a02a985679b44796469cad17bc6
nigel nkomo <nigel.nkomo@icloud.com>
2024-12-11 20:59:38 +0200

    add hero section

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let textAnimation = document.querySelector(".text__animation");
+let spans = textAnimation.innerText
+  .split("")
+  .map((letter, i) => {
+    return `<span style="transition-delay: ${i * 40}ms; filter:hue-rotate(${
+      i * 100
+    }deg)">${letter}</span>`;
+  })
+  .join("");
+console.log(spans);
+
+textAnimation.innerHTML = spans;
+console.log(spans);
