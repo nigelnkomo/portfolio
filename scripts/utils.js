051d5a88116a4f2bb26fabf32c0020bdfbd8171a
nigelnkomo <nigel.nkomo@proton.me>
2025-08-15 17:46:56 +0200

    Change relative asset paths to absolute asset paths

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,52 +1,17 @@
-// Blog posts
-
 const projects = [
   {
     title: "Project One",
-    image: "../assets/imgs/1.jpg",
+    image: "/assets/imgs/1.jpg",
   },
   {
     title: "Project Two",
-    image: "../assets/imgs/2.jpg",
+    image: "/assets/imgs/2.jpg",
   },
   {
     title: "Project Three",
-    image: "../assets/imgs/3.jpg",
+    image: "/assets/imgs/3.jpg",
   },
 ];
-
-// const createProjectCards = () => {
-//     projects.forEach((project) => {
-//         let projectSection = document.createElement("div");
-//         projectSection.classList.add("project-card");
-
-//         let projectDiv = document.createElement("div");
-//         projectDiv.classList.add("project");
-
-//         let imageContainer = document.createElement("div");
-//         imageContainer.classList.add("project-image-container");
-
-//         let image = document.createElement("img");
-//         image.classList.add("project-img");
-//         image.src = project.image;
-
-//         let projectDetails = document.createElement("div");
-//         projectDetails.classList.add("project-details");
-
-//         let projectTitle = document.createElement("h3");
-//         projectTitle.innerText = project.title;
-
-//         let projectInfo = document.createElement("p");
-//         projectInfo.innerText = project.info;
-
-//         imageContainer.appendChild(image);
-//         projectDetails.append(projectTitle, projectInfo);
-//         projectDiv.append(imageContainer, projectDetails);
-//         projectSection.appendChild(projectDiv);
-
-//         document.getElementById("projects").appendChild(projectSection);
-//     });
-// };
 
 const createProjectCards = () => {
   projects.forEach((project) => {
